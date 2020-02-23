16b5468c5c72ec4c322a322707db20a2066cd64b
Ivan Poprotskiy <57532048+Doominiq88@users.noreply.github.com>
2020-02-23 20:08:36 +0300

    fix: close new task

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
-// получение минимального элемента массива
 exports.min = function min(array) {
-    var min = array[0]; // берем первый элемент массива
-    for (var i = 0; i < array.length; i++) { // перебор массива
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    var min = array[0];
+    for (var i = 0; i < array.length; i++) {
+
         if (min > array[i]) min = array[i]
     }
     return min;
 }
-// получение максимального элемента массива
+
 exports.max = function max(array) {
-    var max = array[0]; // берем первый элемент массива
-    for (var i = 0; i < array.length; i++) { // перебор массива
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    var max = array[0];
+    for (var i = 0; i < array.length; i++) {
         if (max < array[i]) max = array[i]
     }
     return max;
 }
-// получение среднего арифметического
+
 exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
     // var arr = [];
     var sum = 0;
     for (let i = 0; i < array.length; i++) {
